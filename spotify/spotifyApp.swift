bb35b96cfa10402ee8323eea63a67a2214c9cfbe
whatchuThinkin <spearhead0427@gmail.com>
2022-11-17 12:22:06 +0900

    Add files via upload

---
+++ spotify/spotifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  spotifyApp.swift
+//  spotify
+//
+//  Created by 1100690 on 2022/11/17.
+//
+
+import SwiftUI
+
+@main
+struct spotifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
